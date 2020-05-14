0832808b3eed694073d35c79dddb133e478e94d4
Naam <naam.pt@gmail.com>
2020-05-14 19:19:15 -0400

    update script

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ const calculator = {
   firstOperand: null,
   isNumberComplete: false,
   mathOperator: null,
-  total: null,
-  previousKey: null
+  previousKey: null,
 };
 
 //---------------------------------------------------------------------------------
@@ -17,6 +16,8 @@ const numberBtns = document.querySelectorAll('.number');
 numberBtns.forEach(button => button.addEventListener('click', getNumber));
 
 function getNumber(e) {
+  //debugger;
+
   //starting
   if (calculator.isNumberComplete === false) {
 
@@ -40,17 +41,14 @@ function getNumber(e) {
   }
 
   calculator.isNumberComplete = false;
+  calculator.previousKey = null;
 
-  updateDisplay()
-  
   log(calculator);
+  updateDisplayScreen();
 }
+//-------------------------------------------------------
 
-//---------------------------------------------------------------------------------
-
-function updateDisplay() {
-  //const display = document.querySelector('.calculator-screen');
-  //update text con
+function updateDisplayScreen() {
   display.textContent = calculator.displayValue;
 
 }
@@ -61,38 +59,67 @@ const operatorBtns = document.querySelectorAll('.operator-key');
 operatorBtns.forEach(button => button.addEventListener('click', getMathOperator))
 
 function getMathOperator(e) {
-  //this make first number complete
+  // this make number complete
   calculator.isNumberComplete = true;
 
-  //update operator to an object
-  calculator.mathOperator = e.target.value;
-
-  displayCalculation();
-
-  //if firstoperand and mathOperator is not null
-  if (calculator.firstOperand && calculator.mathOperator) {
+  if (calculator.firstOperand && calculator.mathOperator && calculator.displayValue) {
     calculate();
+    calculator.mathOperator = e.target.value;
+  } else {
+    calculator.mathOperator = e.target.value;
   }
 
+  displayCalculation()
 
+  calculator.previousKey = 'mathOperator';
   log(calculator);
 
-  //displayCalculation()
-
 }
-
-
-
 //---------------------------------------------------------------------------------
 
+let displayMath = document.querySelector('.display-calculation')
+
+function displayCalculation() {
+  //debugger;
+
+  //this is to prevent pushing mathOperator to an array twice
+  if (calculator.previousKey === 'mathOperator') {
+    calculation.pop()
+    calculation.push(calculator.mathOperator)
+    log(calculation);
+
+    let display = calculation.join(' ');
+    displayMath.textContent = display;
+    return;
+  }
+
+  if (calculator.isNumberComplete === true && calculator.mathOperator) {
+    calculation.push(calculator.displayValue, calculator.mathOperator)
+  }
+
+  let display = calculation.join(' ');
+  displayMath.textContent = display;
+  log(calculation);
+}
+
+//------------------------------------------------------------------
+
 const calculationKey = document.getElementById('calculation-key')
-calculationKey.addEventListener('click', calculate)
+calculationKey.addEventListener('click', calculate);
+
 
 function calculate(e) {
+  //debugger;
+  // case 1 - this is '=' operator
   let firstNum = Number(calculator.firstOperand);
   let secondNum = Number(display.textContent);
   let mathOperator = calculator.mathOperator;
   let result = '';
+
+  if (!(calculator.firstOperand && calculator.mathOperator)) {
+    return;
+  }
+  //debugger;
 
   switch (mathOperator) {
     case '+':
@@ -111,30 +138,62 @@ function calculate(e) {
       result = firstNum /= secondNum
       break;
   }
-  calculator.isNumberComplete = true;
-  calculator.previousKey = e.target.id;
-  log(calculator.previousKey);
-  displayCalculation();
 
   calculator.displayValue = result.toString();
   calculator.firstOperand = '';
-  
+  calculator.mathOperator = '';
 
-  updateDisplay();
-  
+  updateDisplayScreen();
 
   log(calculator);
 }
 
+//-------------------------------------------------------------
+const delBtn = document.getElementById('delete');
 
-//---------------------------------------------------------------------------------
+delBtn.addEventListener('click', e => {
+  calculator.displayValue = calculator.displayValue.slice(0, -1)
 
+  //if str.length = 0 - update display to be 0
+  if (calculator.displayValue.length === 0) {
+    calculator.displayValue = '0';
+  }
 
+  log(calculator);
+  updateDisplayScreen();
+
+});
+
+//-----------------------------------------------------
+
+const clearBtn = document.getElementById('clear');
+clearBtn.addEventListener('click', e => {
+  calculator.displayValue = '0';
+  updateDisplayScreen();
+});
+
+//--------------------------------------------------------
+
+const clearAllBtn = document.getElementById("clear-all");
+
+clearAllBtn.addEventListener("click", (e) => {
+  calculator.displayValue = "0";
+  calculator.firstOperand = null;
+  calculator.mathOperator = null;
+  calculator.isNumberComplete = false;
+  calculator.previousKey = "clear-all";
+
+  updateDisplayScreen();
+
+  calculation = [];
+  displayCalculation();
+});
+
+//-------------------------------------------------------
 const decimalBtn = document.getElementById('decimal-key');
 decimalBtn.addEventListener('click', makeDecimalPoint)
 
 function makeDecimalPoint(e) {
-  //debugger;
   if (calculator.isNumberComplete === false) {
     //continuing number
     if (calculator.displayValue.indexOf(e.target.value) >= 0) {
@@ -160,99 +219,5 @@ function makeDecimalPoint(e) {
 
   calculator.isNumberComplete = false;
 
-  updateDisplay();
+  updateDisplayScreen();
 }
-//-----------------------------------------------------------------
-
-//***if = hit CE/C works the same
-//***else it's different
-
-//CE button
-//if hit this - set display value to '0'
-const clearBtn = document.getElementById('clear');
-
-clearBtn.addEventListener('click', e => {
-  calculator.displayValue = '0';
-  updateDisplay();
-  log(calculator)
-});
-
-//-----------------------------------------------------------------
-
-//if hitted - set display to '0' and everything else to null
-
-const clearAllBtn = document.getElementById('clear-all');
-
-clearAllBtn.addEventListener('click', e => {
-  calculator.displayValue = '0';
-  calculator.firstOperand = null;
-  calculator.mathOperator = null;
-  updateDisplay();
-  log(calculator)
-});
-
-//-----------------------------------------------------------------
-
-//if hitted = the result should not be able to del
-//Del btn
-//if hitted - delete the last digit on the display
-
-const delBtn = document.getElementById('delete');
-
-delBtn.addEventListener('click', e => {
-  //debugger;
-  //grab calculator.display
-  calculator.displayValue = calculator.displayValue.slice(0, -1)
-  
-  //if str.length = 0 - update display to be 0
-  if(calculator.displayValue.length === 0) {
-    calculator.displayValue = '0';
-  }
-
-  log(calculator);
-  updateDisplay();
-  
-});
-//https://stackoverflow.com/questions/51923613/javascript-total-math-of-string-with-multiple-operators-for-js-calculator
-
-
-
-// //grab DOM
-let displayMath = document.querySelector('.display-calculation')
-// //displayMath.textContent = 0
-// //log(displayMath);
-
-function displayCalculation() {
-  //debugger;
-  //if num complete = true
-  //push display to be first number
-  if (calculator.isNumberComplete === true && calculator.previousKey === 'calculation-key') {
-    calculation.push(calculator.displayValue);
-    calculation.push('=');
-
-    //set previous key to null
-    calculator.previousKey = '';
-    let display = calculation.join(' ');
-    displayMath.textContent = display;
-    log(calculation)
-
-    //then clear all items in Array and push total to an array 
-    //calculator = [total]
-    
-    return;
-  }
-
-  if (calculator.isNumberComplete === true) {
-    calculation.push(calculator.displayValue);
-    calculation.push(calculator.mathOperator);
-  }
-  
-  let display = calculation.join(' ');
-  displayMath.textContent = display;
-  log(calculation);
-  
-} 
-
-// function updateDisplay() {
-//   displayMath.textContent = calculation[0];
-// }
