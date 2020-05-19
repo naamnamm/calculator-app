8ca4ab6aca06c892382a3d3078e34aed91359690
Naam <naam.pt@gmail.com>
2020-05-19 16:31:12 -0400

    add styling and update script

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const log = console.log;
-const display = document.querySelector('.display-screen');
 let calculation = [];
 
 const calculator = {
@@ -7,56 +6,50 @@ const calculator = {
   firstOperand: null,
   isNumberComplete: false,
   mathOperator: null,
-  previousKey: null, //maybe need to change this to action
-  storedValue: null
+  storedValue: null,
+  state: null,
+  previousKey: null,
+  isOperatorReady: false
 };
 
 
 
-//---------------------------------------------------------------------------------
 
 const numberBtns = document.querySelectorAll('.number');
 numberBtns.forEach(button => button.addEventListener('click', getNumber));
 
 function getNumber(e) {
   //debugger;
-
-  //starting
   if (calculator.isNumberComplete === false) {
-
     if (calculator.displayValue === '0') {
       calculator.displayValue = e.target.value;
     } else {
       calculator.displayValue += e.target.value;
     }
-
   }
 
   if (calculator.isNumberComplete === true) {
-    //save first number from the display to an object
-    //debugger;
-    calculator.firstOperand = display.textContent
-
-    //replace display with new num 
-    display.textContent = e.target.value;
-    //update display value - calculator object 
-    calculator.displayValue = display.textContent;
-
+    calculator.firstOperand = calculator.displayValue
+    calculator.displayValue = e.target.value;
   }
+
   calculator.isNumberComplete = false;
   calculator.previousKey = 'number';
+  calculator.isOperatorReady = false;
 
   log(calculator);
   updateDisplayScreen(calculator.displayValue);
 
+  if (calculator.state === 'restart') {
+    displayCalculation(calculator.state);
+  }
 }
 //-------------------------------------------------------
 
 function updateDisplayScreen(displayValue) {
+  const display = document.querySelector('.display-screen');
   display.textContent = displayValue;
 }
-
-
 //---------------------------------------------------------------------------------
 
 const operatorBtns = document.querySelectorAll('.operator-key');
@@ -65,70 +58,82 @@ operatorBtns.forEach(button => button.addEventListener('click', getMathOperator)
 function getMathOperator(e) {
   // this makes number complete
   calculator.isNumberComplete = true;
+  //debugger;
 
-  if (calculator.firstOperand && calculator.mathOperator && calculator.displayValue) {
-    calculator.storedValue = calculator.displayValue;
-    let result = performOperation(Number(calculator.firstOperand), Number(calculator.displayValue), calculator.mathOperator);
-    calculator.displayValue = result;
-    updateDisplayScreen(calculator.displayValue)
-    calculator.mathOperator = e.target.value;
-  } else {
-    calculator.mathOperator = e.target.value;
+  if (calculator.isOperatorReady === true) {
+    if (calculator.firstOperand && calculator.mathOperator && calculator.displayValue) {
+      calculator.mathOperator = e.target.value;
 
+      calculator.state = 'continuousCalculation';
+      displayCalculation(calculator.state)
+    } else {
+      calculator.mathOperator = e.target.value;
+      calculator.state = 'initialDisplay';
+      displayCalculation(calculator.state)
+    }
   }
 
-  displayCalculation()
+  if (calculator.isOperatorReady === false) {
+    if (calculator.firstOperand && calculator.mathOperator && calculator.displayValue) {
+      calculator.storedValue = calculator.displayValue;
+
+      let result = performOperation(Number(calculator.firstOperand), Number(calculator.displayValue), calculator.mathOperator);
+      calculator.displayValue = result.toString();
+      updateDisplayScreen(calculator.displayValue);
+
+      calculator.mathOperator = e.target.value;
+
+      calculator.state = 'continuousCalculation';
+      displayCalculation(calculator.state)
+
+    } else {
+      calculator.mathOperator = e.target.value;
+      calculator.state = 'initialDisplay';
+      displayCalculation(calculator.state)
+    }
+  }
 
   calculator.previousKey = 'mathOperator';
-  log(calculator);
+  calculator.isOperatorReady = true;
+  log(calculator)
 
 }
 //---------------------------------------------------------------------------------
 
 let displayMath = document.querySelector('.display-calculation')
 
-function displayCalculation() {
-
-  if (calculator.previousKey === 'calculation-key') {
-    calculation.push(calculator.storedValue);
-    calculation.push('=');
-
-    //set previous key to null
-    calculator.previousKey = '';
-    let display = calculation.join(' ');
-    displayMath.textContent = display;
-    log(calculation)
-    // clear calculation array
-    calculation = [];
-    return;
-  }
-
+function displayCalculation(calculatorState) {
+  //debugger;
   //this is to prevent pushing mathOperator to an array twice
   if (calculator.previousKey === 'mathOperator') {
     calculation.pop()
     calculation.push(calculator.mathOperator)
-    log(calculation);
-    let display = calculation.join(' ');
-    displayMath.textContent = display;
+    displayMath.textContent = calculation.join(' ');
     return;
   }
 
-  if (calculator.storedValue) {
-    calculation.push(calculator.storedValue, calculator.mathOperator);
-    let display = calculation.join(' ');
-    displayMath.textContent = display;
-    log(calculation);
-    return;
+  if (calculatorState === 'restart' || calculatorState === 'clear-all') {
+    calculation = [];
   }
 
-  if (calculator.isNumberComplete === true && calculator.mathOperator) {
+  if (calculatorState === 'initialDisplay') {
+    calculation = [];
     calculation.push(calculator.displayValue, calculator.mathOperator);
-    let display = calculation.join(' ');
-    displayMath.textContent = display;
-    log(calculation);
   }
+
+  if (calculatorState === 'continuousCalculation') {
+    calculation.push(calculator.storedValue, calculator.mathOperator);
+  }
+
+  if (calculatorState === 'finalCalculation') {
+    calculation.push(calculator.storedValue, '=');
+  }
+
+  displayMath.textContent = calculation.join(' ');
+  log(calculation)
 
   calculationKey.storedValue = null;
+  calculator.state = null;
 
 }
 
@@ -137,6 +142,8 @@ function displayCalculation() {
 // }
 //if num complete = true
 //push display to be first number
+
+
 
 
 //------------------------------------------------------------------
@@ -152,18 +159,20 @@ function calculate(e) {
   let result = performOperation(firstNum, secondNum, mathOperator)
 
   if (!(calculator.firstOperand && calculator.mathOperator)) {
-    return;
   }
 
   calculator.storedValue = calculator.displayValue;
   calculator.displayValue = result.toString();
   calculator.firstOperand = '';
   calculator.mathOperator = '';
-  calculator.previousKey = 'calculation-key';
+  calculator.state = 'finalCalculation'
+  calculator.isNumberComplete = true;
 
   updateDisplayScreen(calculator.displayValue);
-  displayCalculation();
+  displayCalculation(calculator.state);
   log(calculator);
+
+  calculator.state = 'restart';
 }
 
 function performOperation(firstNum, secondNum, mathOperator) {
@@ -205,28 +214,42 @@ delBtn.addEventListener('click', e => {
 
 //-----------------------------------------------------
 
-const clearBtn = document.getElementById('clear');
+const clearBtn = document.getElementById('CE');
 clearBtn.addEventListener('click', e => {
   calculator.displayValue = '0';
+
   updateDisplayScreen(calculator.displayValue);
+
+  if (calculator.state === 'restart') {
+    displayCalculation(calculator.state);
+  }
+
 });
+
+
 
 //--------------------------------------------------------
 
-const clearAllBtn = document.getElementById("clear-all");
+//CE & E works the same when state = restart
+//otherwise C > clear all everytime
+//CE > just make display value to be 0
+
+const clearAllBtn = document.getElementById("C");
 
 clearAllBtn.addEventListener("click", (e) => {
   calculator.displayValue = "0";
   calculator.firstOperand = null;
   calculator.mathOperator = null;
   calculator.isNumberComplete = false;
-  calculator.previousKey = "clear-all";
+  calculator.state = 'clear-all';
 
+  //this make display value 0 anyway
   updateDisplayScreen(calculator.displayValue);
-
-  calculation = [];
-  displayCalculation();
+  //if state is restart > clear all
+  displayCalculation(calculator.state);
 });
+
+
 
 //-------------------------------------------------------
 const decimalBtn = document.getElementById('decimal-key');
@@ -250,7 +273,7 @@ function makeDecimalPoint(e) {
   //after hit operand > get second Number
   if (calculator.isNumberComplete === true) {
     //save first number from the display to an object
-    calculator.firstOperand = display.textContent
+    calculator.firstOperand = calculator.displayValue
 
     //replace display with new num 
     calculator.displayValue = '0' + e.target.value;
@@ -259,4 +282,30 @@ function makeDecimalPoint(e) {
   calculator.isNumberComplete = false;
 
   updateDisplayScreen(calculator.displayValue);
+
+  if (calculator.state === 'restart') {
+    displayCalculation(calculator.state);
+  }
 }
+
+//------------------------------
+const plusMinusKeyEl = document.getElementById('plus-minus');
+plusMinusKeyEl.addEventListener('click', plusMinusNumber);
+
+function plusMinusNumber(e) {
+
+  //debugger;
+  if (calculator.displayValue > 0) {
+    calculator.displayValue = (-Math.abs(calculator.displayValue)).toString();
+  } else {
+    calculator.displayValue = Math.abs(calculator.displayValue).toString();
+  }
+
+  updateDisplayScreen(calculator.displayValue);
+
+  if (calculator.state === 'restart') {
+    displayCalculation(calculator.state);
+  }
+
+}
+
